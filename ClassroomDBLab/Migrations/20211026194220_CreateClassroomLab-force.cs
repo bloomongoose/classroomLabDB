cf663f4936cd7b5d891e8001342c4f18c8c361e2
radeen <radeen013@gmail.com>
2021-10-27 09:02:04 -0400

    lab

---
+++ ClassroomDBLab/Migrations/20211026194220_CreateClassroomLab-force.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClassroomDBLab.Migrations
+{
+    public partial class CreateClassroomLabforce : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Food",
+                table: "Students",
+                newName: "favFood");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "favFood",
+                table: "Students",
+                newName: "Food");
+        }
+    }
+}
---
+++ ClassroomDBLab/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ClassroomDBLab
+{
+    class Student
+    {
+        //properties
+        [Key]
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public string favFood { get; set; }
+        public string Hometown { get; set; }
+
+    }   
+}
